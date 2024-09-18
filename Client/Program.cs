938eb10dc2def04c374bbcab148f932adae749fd
Nazar Melnik <115361939+Arlikey@users.noreply.github.com>
2024-09-18 11:09:14 +0300

    pushed client file

---
+++ Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var connection = new HubConnectionBuilder().WithUrl("https://localhost:7196/exchangeHub").Build();
+
+        connection.On<string, decimal>("ReceiveCurrencyUpdate", (currencyPair, rate) =>
+        {
+            Console.WriteLine($"Rate for {currencyPair} : {rate}");
+        });
+
+        await connection.StartAsync();
+        Console.WriteLine("Connected to server.");
+        Console.ReadLine();
+    }
+}
